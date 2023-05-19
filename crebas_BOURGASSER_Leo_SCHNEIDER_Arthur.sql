7eeb20f3f8a83ac6b1a816c101faaaa3e495fbf5
Léo Bourgasser <leo.bourgasser@etudiant.univ-reims.fr>
2023-05-19 15:13:16 +0200

    MaJ script de création

--- crebas_BOURGASSER_Leo_SCHNEIDER_Arthur.sql
+++ crebas_BOURGASSER_Leo_SCHNEIDER_Arthur.sql
@@ -29,22 +29,6 @@ drop table TERRITOIRE cascade constraints;
 drop table THEME cascade constraints;
 
 /*==============================================================*/
-/* Table : EVENEMENT                                            */
-/*==============================================================*/
-create table EVENEMENT 
-(
-   cdPers               INTEGER              not null,
-   numEv                INTEGER              not null,
-   dateDebEv            DATE                 not null,
-   dateFinEv            DATE,
-   nbPlaces             INTEGER,
-   tarif                FLOAT,
-   constraint PK_EVENEMENT primary key (cdPers, numEv),
-   constraint Contrainte_mini_nbPlace check (nbPlaces >= 20),
-   constraint CK_dateFinEv check (dateFinEv >= dateDebEv)
-);
-
-/*==============================================================*/
 /* Table : PARTICIPANT                                          */
 /*==============================================================*/
 create table PARTICIPANT 
@@ -63,39 +47,21 @@ create table PARTICIPANT
 );
 
 /*==============================================================*/
-/* Table : RESERVATION                                          */
+/* Table : EVENEMENT                                            */
 /*==============================================================*/
-create table RESERVATION 
+create table EVENEMENT 
 (
    cdPers               INTEGER              not null,
-   EVE_cdPers           INTEGER              not null,
    numEv                INTEGER              not null,
-   cdSite               INTEGER              not null,
-   dateResa             DATE                 not null,
-   nbPlResa             INTEGER,
-   modeReglt            INTEGER,
-   constraint PK_RESERVATION primary key (cdPers, EVE_cdPers, numEv, cdSite, dateResa),
-   constraint Contrainte_modeReglt check (1 <= modeReglt <= 3
-)
-);
-
-/*==============================================================*/
-/* Table : SITE                                                 */
-/*==============================================================*/
-create table SITE 
-(
-   cdSite               INTEGER              not null,
-   cdTerr               INTEGER              not null,
-   cdTheme              INTEGER              not null,
-   nomSite              VARCHAR2(100),
-   tpSite               VARCHAR2(100),
-   adrSite              VARCHAR2(100),
-   cpSite               CHAR(5),
-   villeSite            VARCHAR2(100),
-   emailSite            VARCHAR2(100),
-   telSite              CHAR(10),
-   siteweb              VARCHAR2(100),
-   constraint PK_SITE primary key (cdSite)
+   dateDebEv            DATE                 not null,
+   dateFinEv            DATE,
+   nbPlaces             INTEGER,
+   tarif                FLOAT,
+   constraint PK_EVENEMENT primary key (cdPers, numEv),
+   constraint Contrainte_mini_nbPlace check (nbPlaces >= 20),
+   constraint CK_dateFinEv check (dateFinEv >= dateDebEv),
+   constraint FK_EVENEMEN_PARTICIPE_PARTICIP foreign key (cdPers)
+      references PARTICIPANT (cdPers)
 );
 
 /*==============================================================*/
@@ -118,27 +84,49 @@ create table THEME
    constraint PK_THEME primary key (cdTheme)
 );
 
-alter table EVENEMENT
-   add constraint FK_EVENEMEN_PARTICIPE_PARTICIP foreign key (cdPers)
-      references PARTICIPANT (cdPers);
+/*==============================================================*/
+/* Table : SITE                                                 */
+/*==============================================================*/
+create table SITE 
+(
+   cdSite               INTEGER              not null,
+   cdTerr               INTEGER              not null,
+   cdTheme              INTEGER              not null,
+   nomSite              VARCHAR2(100),
+   tpSite               VARCHAR2(100),
+   adrSite              VARCHAR2(100),
+   cpSite               CHAR(5),
+   villeSite            VARCHAR2(100),
+   emailSite            VARCHAR2(100),
+   telSite              CHAR(10),
+   siteweb              VARCHAR2(100),
+   constraint PK_SITE primary key (cdSite),
+   constraint FK_SITE_CORRESPON_THEME foreign key (cdTheme)
+      references THEME (cdTheme),
+   constraint FK_SITE_APPARTENI_TERRITOI foreign key (cdTerr)
+      references TERRITOIRE (cdTerr)
+);
 
-alter table RESERVATION
-   add constraint FK_RESERVAT_COINCIDER_EVENEMEN foreign key (EVE_cdPers, numEv)
-      references EVENEMENT (cdPers, numEv);
+/*==============================================================*/
+/* Table : RESERVATION                                          */
+/*==============================================================*/
+create table RESERVATION 
+(
+   cdPers               INTEGER              not null,
+   EVE_cdPers           INTEGER              not null,
+   numEv                INTEGER              not null,
+   cdSite               INTEGER              not null,
+   dateResa             DATE                 not null,
+   nbPlResa             INTEGER,
+   modeReglt            INTEGER,
+   constraint PK_RESERVATION primary key (cdPers, EVE_cdPers, numEv, cdSite, dateResa),
+   constraint Contrainte_modeReglt check (modeReglt BETWEEN 1 and 3),
+   constraint FK_RESERVAT_RESERVER_PARTICIP foreign key (cdPers)
+      references PARTICIPANT (cdPers),
+   constraint FK_RESERVAT_CONSERNER_SITE foreign key (cdSite)
+      references SITE (cdSite),
+   constraint FK_RESERVAT_COINCIDER_EVENEMEN foreign key (EVE_cdPers, numEv)
+      references EVENEMENT (cdPers, numEv)
+);
 
-alter table RESERVATION
-   add constraint FK_RESERVAT_CONSERNER_SITE foreign key (cdSite)
-      references SITE (cdSite);
-
-alter table RESERVATION
-   add constraint FK_RESERVAT_RESERVER_PARTICIP foreign key (cdPers)
-      references PARTICIPANT (cdPers);
-
-alter table SITE
-   add constraint FK_SITE_APPARTENI_TERRITOI foreign key (cdTerr)
-      references TERRITOIRE (cdTerr);
-
-alter table SITE
-   add constraint FK_SITE_CORRESPON_THEME foreign key (cdTheme)
-      references THEME (cdTheme);
 
